2ebfebeda2aa5182fd5c7cdb6e2ca36195966f57
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-08-09 16:33:42 -0300

    atualização

--- frontend/src/Pages/Discover/index.jsx
+++ frontend/src/Pages/Discover/index.jsx
@@ -24,7 +24,7 @@ export default () => {
 
             <FIlters type={type} setType={setType} selectedCetegories={selectedCetegories} setSelectedCategories={setSelectedCategories} />
 
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 mx-[30px]">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 mx-[30px]">
                 {moviesList.map((movie) => (
                     <Link 
                         to={`/detail/${type}/${movie.id}`}
