f95db6378e1eca1191e4ac1ae8770b2451d10bc0
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-08-14 13:20:39 -0300

    atualização

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom"
 import SearchIcon from '@mui/icons-material/Search';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 
 export default () => {
+    const [backgroundHeader, setBackgroundHeader] = useState(false)
+
+    useEffect(() => {
+        const scrollListener = () => {
+            if (window.scrollY > 10) {
+                setBackgroundHeader(true)
+            } else {
+                setBackgroundHeader(false)
+            }
+        }
+
+        window.addEventListener('scroll', scrollListener)
+
+        return () => {
+            window.removeEventListener('scroll', scrollListener)
+        }
+    }, [])
 
     return (
-        <header className="fixed top-0 left-0 right-0 z-50 px-[30px] py-2 flex items-center justify-between bg-black/50 backdrop-blur-md border-b border-white/10 shadow-lg">
+        <header className={`fixed top-0 left-0 right-0 z-50 px-[30px] py-2 flex items-center justify-between transition duration-500 ${backgroundHeader && 'bg-black/50 backdrop-blur-md border-b border-white/10 shadow-lg'}`}>
 
             <Link to={"/"}>
                 <h1 className="text-2xl font-bold text-blue-500 tracking-wide">
