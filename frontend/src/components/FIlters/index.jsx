cfe7bfb89140c572b872c54760ded944486fb312
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-08-15 14:09:27 -0300

    atualização

--- frontend/src/components/FIlters/index.jsx
+++ frontend/src/components/FIlters/index.jsx
@@ -30,7 +30,7 @@ export default ({ type, setType, selectedCetegories, setSelectedCategories }) =>
                 <button 
                     className={`px-4 py-2 rounded-full border transition cursor-pointer 
                     ${selectedType == 'filme' 
-                        ? "bg-blue-500 border-blue-500 text-white"
+                        ? "bg-blue-600 border-blue-600 text-white"
                         : "bg-transparent border-gray-500 text-gray-300 hover:bg-gray-700 hover:border-gray-400"
                     }`}
                     onClick={() => {setType('movie'); setSelectedType('filme'); setSelectedCategories([])}}>Filme</button>
@@ -38,7 +38,7 @@ export default ({ type, setType, selectedCetegories, setSelectedCategories }) =>
                 <button 
                     className={`px-4 py-2 rounded-full border transition cursor-pointer 
                     ${selectedType == 'serie' 
-                        ? "bg-blue-500 border-blue-500 text-white"
+                        ? "bg-blue-600 border-blue-600 text-white"
                         : "bg-transparent border-gray-500 text-gray-300 hover:bg-gray-700 hover:border-gray-400"
                     }`}
                     onClick={() => {setType('tv'); setSelectedType('serie'); setSelectedCategories([])}}>Série</button>
@@ -54,7 +54,7 @@ export default ({ type, setType, selectedCetegories, setSelectedCategories }) =>
                             onClick={() => toggleCategory(category.id)}
                             className={`px-4 py-2 rounded-full border transition cursor-pointer 
                             ${selectedCetegories.includes(category.id)
-                                ? "bg-blue-500 border-blue-500 text-white" 
+                                ? "bg-blue-600 border-blue-600 text-white" 
                                 : "bg-transparent border-gray-500 text-gray-300 hover:bg-gray-700 hover:border-gray-400"
                             }`}
                         >
